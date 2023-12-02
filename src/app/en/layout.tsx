b986948a8058aee6fe467a24425aa60e7ede210f
irwNd2 <irwandduadua@gmail.com>
2023-12-03 00:00:45 +0700

    feat: sign up page

--- src/app/en/layout.tsx
+++ src/app/en/layout.tsx
@@ -1,9 +1,13 @@
-const HomePageLayout = ({
-  children,
-}: {
-  children: React.ReactNode;
-}) => {
-  return <div>{children}</div>;
+
+import Header from "@/components/landing-page/header";
+
+const HomePageLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main>
+      <Header />
+      {children}
+    </main>
+  );
 };
 
 export default HomePageLayout;
